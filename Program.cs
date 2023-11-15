ced65c4bb4f327f01a7aaaab924df714f676ec28
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-15 09:30:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraznanja4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite dvije rijeci: ");
+            string tekst = Console.ReadLine();
+            string[] rijeci = tekst.Split(' ');
+            Console.WriteLine("vaš redosljed: " + rijeci[0] +" "+ rijeci[1]);
+            Console.WriteLine(rijeci[0].ToCharArray());
+            Array.Sort(rijeci);
+            Console.WriteLine(rijeci);
+            Console.ReadKey();
+        }
+    }
+}
